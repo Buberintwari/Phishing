f1d3101af6a10752bf80d41d053d479466a509aa
Buberintwari <emerypatience@gmail.com>
2025-04-19 18:18:30 +0100

    ngrok

--- phishing_project/phishing_project/settings.py
+++ phishing_project/phishing_project/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-r&pexdhg8j8wa5zhn@1*t29^1_g=rpx=^s&m8h)4re7dw_7gqv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.ngrok-free.app'
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.ngrok-free.app',
+]
 
 
 # Application definition
--- phishing_project/phishing_project/urls.py
+++ phishing_project/phishing_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('phishing/', include('phishing_simulation.urls')),
+    path('github/', include('phishing_simulation.urls')),
 ]
